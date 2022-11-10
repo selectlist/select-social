072748ab2bd01a9adca45c8ddbce5a1ec847063c
selectdev <teamviewselect@gmail.com>
2022-11-09 22:34:56 -0600

    Now i actually fixed it (maybe)

--- pages/_app.js
+++ pages/_app.js
@@ -137,7 +137,7 @@ const SelectSocial = ({ Component, pageProps }) => {
 									</div>
 								</div>
 
-								{this.props.user ? (
+								{pageProps.user ? (
 									<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 										<button
 											type="button"
