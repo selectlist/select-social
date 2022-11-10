1374db806ff3e82c7230a6c22327f1c882e4705b
selectdev <teamviewselect@gmail.com>
2022-11-09 22:31:52 -0600

    Fixed props issue

--- pages/_app.js
+++ pages/_app.js
@@ -137,7 +137,7 @@ const SelectSocial = ({ Component, pageProps }) => {
 									</div>
 								</div>
 
-								{this.pageProps.user ? (
+								{this.props.user ? (
 									<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 										<button
 											type="button"
